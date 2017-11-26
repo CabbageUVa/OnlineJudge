68c0c9901b9e7ebab83f25fd41cdf13bea875b47
cabbageUva <fang3383@gmail.com>
2017-11-26 04:23:36 -0500

    distinguish signup error code

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, make_response, render_template
 from jwt.exceptions import ExpiredSignatureError, DecodeError
 from datetime import datetime, timedelta
 from flaskext.mysql import MySQL
-#from comp_run import test
+# from comp_run import test
 import os, string, random, jwt, time
 
 app = Flask(__name__)
@@ -25,8 +25,6 @@ def index():
 
 @app.route('/login', methods=['POST'])
 def userLogin():
-    t = time.time()
-    print t
     # login code :
     # 200 OK, 201 wrong password, 202 unknown error
     if request.method == 'POST':
@@ -36,34 +34,28 @@ def userLogin():
         try:
             if _username and _password:
                 # all good, validate userinfo
-                print time.time() - t
-
                 cursor = conn.cursor()
-                cursor.callproc('sp_validUser',(_username,_password))
+                cursor.callproc('sp_validUser', (_username, _password))
                 data = cursor.fetchall()
-                print time.time() - t
                 if len(data) != 0:
-                # validate successfully
+                    # validate successfully
                     userID = data[0][1]
                     token = register_token(userID)
-                    response = make_response(jsonify(code = '200'))
-                    response.set_cookie('token', token)
-                    response.set_cookie('userID', str(userID))
-                    response.set_cookie('userName', _username)
+                    response = make_response(jsonify(code='200'))
+                    response.set_cookie('token', token, max_age = 1000*60*30)
+                    response.set_cookie('userID', str(userID), max_age = 1000*60*30)
+                    response.set_cookie('userName', _username, max_age = 1000*60*30)
                 else:
                     # incorrect info
-                    response = make_response(jsonify(code = '201'))
-
+                    response = make_response(jsonify(code='201'))
                 response = set_header(response)
-                print time.time() - t
                 return response
         except Exception as e:
-            response = make_response(jsonify(code = '202'))
+            response = make_response(jsonify(code='202'))
             response = set_header(response)
             return response
         finally:
-            cursor.close() 
-            #conn.close()
+            cursor.close()
 
 
 @app.route("/logout")
@@ -72,14 +64,14 @@ def logout():
         userID = request.cookies.get('userID')
         token = request.cookies.get('token')
         valid = validate_user(userID, token)
-        response = make_response(jsonify(code = valid))
+        response = make_response(jsonify(code=valid))
         if valid == 200:
             response.set_cookie('token', token, max_age=0)
         response = set_header(response)
         return response
 
 
-@app.route('/signUp', methods=['POST','GET'])
+@app.route('/signUp', methods=['POST', 'GET'])
 def signUp():
     if request.method == 'POST':
         data = request.form
@@ -91,31 +83,34 @@ def signUp():
             if _name and _email and _password:
                 # All Good, let's call MySQL
                 cursor = conn.cursor()
-                cursor.callproc('sp_createUser',(_name,_password,_email))
+                cursor.callproc('sp_createUser', (_name, _password, _email))
                 data = cursor.fetchall()
-
-                if len(data) != 0 and data[0][0] != -1:
+                if data[0][0] > 0:
                     # registered successfully
                     userID = data[0][0]
                     conn.commit()
                     token = register_token(userID)
-                    response = make_response(jsonify(code = '200'))
-                    response.set_cookie('token', token, max_age = 60*60*1000)
-                    response.set_cookie('userID', str(userID), max_age = 60*60*1000)
-                else:
-                    response = make_response(jsonify(code = '201'))
-
+                    response = make_response(jsonify(code='200'))
+                    response.set_cookie('token', token, max_age=30 * 60 * 1000)
+                    response.set_cookie('userName', _name, max_age=1000 * 60 * 30)
+                    response.set_cookie('userID', str(userID), max_age=30 * 60 * 1000)
+                elif data[0][0] == 0:
+                    # existing username
+                    response = make_response(jsonify(code='201'))
+                elif data[0][0] == -1:
+                    #existing email
+                    response = make_response(jsonify(code='202'))
                 response = set_header(response)
                 return response
         except Exception as e:
-            response = make_response(jsonify(code = '202'))
+            response = make_response(jsonify(code='203'))
             response = set_header(response)
             return response
         finally:
             cursor.close()
 
 
-@app.route('/uploader', methods = ['GET', 'POST'])
+@app.route('/uploader', methods=['GET', 'POST'])
 def compile():
     if request.method == 'POST':
         userID = request.cookies.get('userID')
@@ -126,18 +121,19 @@ def compile():
 
         valid = validate_user(userID, token)
         if valid != 200:
-            response = make_response(jsonify(code = valid))
+            response = make_response(jsonify(code=valid))
         else:
             cursor = conn.cursor()
-            #result = test(code,Q_ID,cursor)
+            # result = test(code,Q_ID,cursor)
             result = "TEST!"
             cursor.close()
             token = register_token(userID)
-            response = make_response(jsonify(code = valid, result = result))
-            response.set_cookie('token', token, max_age = 60*60*1000)
-            response.set_cookie('userID', userID, max_age = 60*60*1000)
+            response = make_response(jsonify(code=valid, result=result))
+            response.set_cookie('token', token, max_age=60 * 60 * 1000)
+            response.set_cookie('userID', str(userID), max_age=60 * 60 * 1000)
         response = set_header(response)
         return response
+
 
 def validate_user(userID, token):
     # status code:
@@ -152,18 +148,21 @@ def validate_user(userID, token):
         return 201
     except DecodeError as e:
         return 202
-        
+
+
 def register_token(userID):
     token = jwt.encode({'userID': userID,
                         'exp': datetime.utcnow() + timedelta(minutes=app.config.get('HAPYAK_JWT_LIFETIME', 60))},
                        app.config.get('JWT_KEY', SECRET_KEY))
     return token
 
+
 def set_header(resp):
     resp.headers['Access-Control-Allow-Origin'] = '*'
     resp.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
     resp.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
     return resp
+
 
 if __name__ == '__main__':
     app.run(host='localhost')
